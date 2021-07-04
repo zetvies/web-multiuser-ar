a9290675d14178c8153dc2221f716de4a91b6f43
Bimo Arsa <zetvies@gmail.com>
2021-07-04 11:03:34 +0700

    Different Folder

--- index.js
+++ index.js
@@ -2,9 +2,7 @@ const app = require('express')();
 
 let PORT=process.env.PORT||3000;
 
-app.get("/", (req,res)=>{
-    res.send("Hello World")
-})
+app.use(express.static("./public_html"));
 
 app.listen(PORT,()=>{
     console.log(`listening on port ${PORT}`)
