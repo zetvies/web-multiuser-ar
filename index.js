1b995cfd7ad3505222bc1fecd0bad8f91c1f2a13
Bimo Arsa <zetvies@gmail.com>
2021-07-04 11:05:50 +0700

    Different Folder

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const app = require('express')();
 
 let PORT=process.env.PORT||3000;
 
-app.use(express.static("./public_html"));
+var htmlPath = path.join(__dirname, 'html');
+app.use(express.static("htmlPath"));
 
 app.listen(PORT,()=>{
     console.log(`listening on port ${PORT}`)
